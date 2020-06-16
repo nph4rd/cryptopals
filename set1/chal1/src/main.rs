75cf8d77c3761adfa78fea18cadd49f8682ffdc3
Arturo Marquez <arturomf94@gmail.com>
2020-06-16 14:45:24 -0500

    add assert

--- set1/chal1/src/main.rs
+++ set1/chal1/src/main.rs
@@ -12,6 +12,7 @@ fn hex2base64(hex: &str) -> String {
 fn main() {
     let hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
     let base64 = hex2base64(hex);
+    assert_eq!(base64, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
     println!("hex: {}", hex);
     println!("base64 {}", base64);
 }
