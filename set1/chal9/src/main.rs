63d9ab5368159f77eb81a6756fe692e4b007b82a
Arturo Marquez <arturomf94@gmail.com>
2020-07-20 10:25:44 -0500

    pkcs7 func takes a slice instead of vec

--- set1/chal9/src/main.rs
+++ set1/chal9/src/main.rs
@@ -1,4 +1,5 @@
-fn pkcs7(mut message: Vec<u8>, block_size: usize) -> Vec<u8> {
+fn pkcs7(message: &[u8], block_size: usize) -> Vec<u8> {
+    let mut message = message.to_vec();
     let mut pading_len = block_size - (message.len() % block_size);
     if pading_len == 0 { pading_len = block_size};
     let pad = vec![pading_len as u8; pading_len as usize];
@@ -8,6 +9,6 @@ fn pkcs7(mut message: Vec<u8>, block_size: usize) -> Vec<u8> {
 
 fn main() {
     let message = b"YELLOW SUBMARINE";
-    let padded_message = pkcs7(message.to_vec(), 20);
+    let padded_message = pkcs7(message, 20);
     assert_eq!(padded_message.as_slice(), b"YELLOW SUBMARINE\x04\x04\x04\x04");
 }
