7b71533adb8ea427118d1d93c8f2abbf9cf88732
Arturo Marquez <arturomf94@gmail.com>
2020-07-25 20:28:54 -0500

    Add xor function and fix split_blocks function

--- set1/chal10/src/main.rs
+++ set1/chal10/src/main.rs
@@ -6,8 +6,17 @@ use std::fs::read_to_string;
 
 const BLOCK_SIZE: usize = 16;
 
+fn xor(x: &Vec<u8>, y: &Vec<u8>) -> Vec<u8> {
+    assert_eq!(x.len(), y.len());
+    let mut xor: Vec<u8> = Vec::new();
+    for i in 0..x.len() {
+        xor.push(x[i] ^ y[i])
+    }
+    xor
+}
+
 fn decrypt_aes_128_ecb(
-    ciphertext: Vec<u8>,
+    ciphertext: &[u8],
     key: &[u8],
 ) -> Vec<u8> {
     let mut decrypter = Crypter::new(
@@ -15,6 +24,7 @@ fn decrypt_aes_128_ecb(
         Mode::Decrypt,
         key,
         None).unwrap();
+    decrypter.pad(false);
     let data_len = ciphertext.len();
     let mut plaintext = vec![0; data_len + BLOCK_SIZE];
     let mut count = decrypter
@@ -27,31 +37,37 @@ fn decrypt_aes_128_ecb(
     plaintext
 }
 
-fn split_blocks<'a>(
-    ciphertext: &'a [u8],
+fn split_blocks(
+    ciphertext: Vec<u8>,
     blocksize: usize,
-) -> Vec<&[u8]> {
-    let mut result: Vec<&[u8]> = Vec::with_capacity(blocksize);
-    for i in 0..blocksize {
-        result.push(&ciphertext[i..(blocksize * (i + 1))]);
+) -> Vec<Vec<u8>> {
+    let num_blocks = ciphertext.len() / blocksize;
+    let mut result: Vec<Vec<u8>> = Vec::with_capacity(num_blocks);
+    for i in 0..num_blocks {
+        result.push(ciphertext[(blocksize * i)..(blocksize * (i + 1))].to_vec());
     }
     result
 }
 
 fn decrypt_aes_128_cbc(
-    ciphertext: &[u8],
+    ciphertext: Vec<u8>,
     iv: Vec<u8>,
     key: &[u8],
 ) -> String {
-    let mut plaintext: Vec<u8> = Vec::with_capacity(ciphertext.len());
+    let ciphertext_len = ciphertext.len();
+    let mut plaintext: Vec<u8> = Vec::with_capacity(ciphertext_len);
     let split_ciphertext = split_blocks(ciphertext, BLOCK_SIZE);
+    let mut prev_block_ciphertxt = iv;
     for i in 0..split_ciphertext.len(){
-        let plaintext_block = decrypt_aes_128_ecb(
-            split_ciphertext[i].to_vec(),
-            key,
+        let current_block = decrypt_aes_128_ecb(
+            &split_ciphertext[i],
+            &key,
         );
-       plaintext.extend(plaintext_block);
+        let plaintext_block = xor(&current_block, &prev_block_ciphertxt);
+        plaintext.extend(plaintext_block);
+        prev_block_ciphertxt = split_ciphertext[i].to_vec();
     }
+    plaintext.truncate(ciphertext_len);
     String::from_utf8(plaintext).unwrap()
 }
 
@@ -62,11 +78,6 @@ fn main() {
         .expect("Invalid base64 input");
     let key = b"YELLOW SUBMARINE";
     let iv = vec![0; BLOCK_SIZE];
-    let plaintext = decrypt_aes_128_cbc(&ciphertext[..], iv, key);
+    let plaintext = decrypt_aes_128_cbc(ciphertext, iv, key);
     println!("{}", plaintext);
-}
-
-#[test]
-fn test_chal10() {
-    assert!(true);
 }
