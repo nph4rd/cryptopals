2db12a54f9f29eba33292e617ad2506a7edf8154
Arturo Marquez <arturomf94@gmail.com>
2020-07-26 12:17:29 -0500

    Use slices and test cbc encrypy/decrypt

--- set1/chal10/src/main.rs
+++ set1/chal10/src/main.rs
@@ -6,9 +6,9 @@ use std::fs::read_to_string;
 
 const BLOCK_SIZE: usize = 16;
 
-fn xor(x: &Vec<u8>, y: &Vec<u8>) -> Vec<u8> {
+fn xor(x: &[u8], y: &[u8]) -> Vec<u8> {
     assert_eq!(x.len(), y.len());
-    let mut xor: Vec<u8> = Vec::new();
+    let mut xor: Vec<u8> = Vec::with_capacity(x.len());
     for i in 0..x.len() {
         xor.push(x[i] ^ y[i])
     }
@@ -37,26 +37,48 @@ fn decrypt_aes_128_ecb(
     plaintext
 }
 
-fn split_blocks(
-    ciphertext: Vec<u8>,
+fn encrypt_aes_128_ecb(
+    plaintext: &[u8],
+    key: &[u8],
+) -> Vec<u8> {
+    let mut encrypter = Crypter::new(
+        Cipher::aes_128_ecb(),
+        Mode::Encrypt,
+        key,
+        None).unwrap();
+    encrypter.pad(false);
+    let data_len = plaintext.len();
+    let mut ciphertext = vec![0; data_len + BLOCK_SIZE];
+    let mut count = encrypter
+        .update(&plaintext[..data_len], &mut ciphertext)
+        .unwrap();
+    count += encrypter
+        .finalize(&mut ciphertext[count..])
+        .unwrap();
+    ciphertext.truncate(count);
+    ciphertext
+}
+
+fn split_blocks<'a>(
+    ciphertext: &'a [u8],
     blocksize: usize,
-) -> Vec<Vec<u8>> {
+) -> Vec<&'a [u8]> {
     let num_blocks = ciphertext.len() / blocksize;
-    let mut result: Vec<Vec<u8>> = Vec::with_capacity(num_blocks);
+    let mut result: Vec<&[u8]> = Vec::with_capacity(num_blocks);
     for i in 0..num_blocks {
-        result.push(ciphertext[(blocksize * i)..(blocksize * (i + 1))].to_vec());
+        result.push(&ciphertext[(blocksize * i)..(blocksize * (i + 1))]);
     }
     result
 }
 
 fn decrypt_aes_128_cbc(
-    ciphertext: Vec<u8>,
-    iv: Vec<u8>,
+    ciphertext: &[u8],
+    iv: &[u8],
     key: &[u8],
-) -> String {
+) -> Vec<u8> {
     let ciphertext_len = ciphertext.len();
     let mut plaintext: Vec<u8> = Vec::with_capacity(ciphertext_len);
-    let split_ciphertext = split_blocks(ciphertext, BLOCK_SIZE);
+    let split_ciphertext = split_blocks(&ciphertext, BLOCK_SIZE);
     let mut prev_block_ciphertxt = iv;
     for i in 0..split_ciphertext.len(){
         let current_block = decrypt_aes_128_ecb(
@@ -65,10 +87,32 @@ fn decrypt_aes_128_cbc(
         );
         let plaintext_block = xor(&current_block, &prev_block_ciphertxt);
         plaintext.extend(plaintext_block);
-        prev_block_ciphertxt = split_ciphertext[i].to_vec();
+        prev_block_ciphertxt = split_ciphertext[i];
     }
     plaintext.truncate(ciphertext_len);
-    String::from_utf8(plaintext).unwrap()
+    plaintext
+}
+
+#[allow(dead_code)]
+fn encrypt_aes_128_cbc(
+    plaintext: &[u8],
+    iv: &[u8],
+    key: &[u8],
+) -> Vec<u8> {
+    let plaintext_len = plaintext.len();
+    let mut ciphertext: Vec<u8> = Vec::with_capacity(plaintext_len);
+    let split_plaintext = split_blocks(&plaintext, BLOCK_SIZE);
+    let mut prev_block_plaintxt = iv;
+    for i in 0..split_plaintext.len(){
+        let cipher_input = xor(&split_plaintext[i], &prev_block_plaintxt);
+        let current_block = encrypt_aes_128_ecb(
+            &cipher_input[..],
+            &key,
+        );
+        ciphertext.extend(current_block);
+        prev_block_plaintxt = &ciphertext[..BLOCK_SIZE];
+    }
+    ciphertext
 }
 
 fn main() {
@@ -78,6 +122,23 @@ fn main() {
         .expect("Invalid base64 input");
     let key = b"YELLOW SUBMARINE";
     let iv = vec![0; BLOCK_SIZE];
-    let plaintext = decrypt_aes_128_cbc(ciphertext, iv, key);
+    let plaintext_vec = decrypt_aes_128_cbc(&ciphertext[..], &iv[..], key);
+    let plaintext = String::from_utf8(plaintext_vec).unwrap();
     println!("{}", plaintext);
+}
+
+#[test]
+fn test_cbc_encrypt_decrypt() {
+    let msg = b"YELLOW SUBMARINEYELLOW SUBMARINE";
+    let key = b"YELLOW SUBMARINE";
+    let iv = vec![0; BLOCK_SIZE];
+    assert_eq!(
+        msg,
+        decrypt_aes_128_cbc(
+            encrypt_aes_128_cbc(msg, &iv[..], key).as_slice(),
+            &iv[..],
+            key,
+        ).as_slice()
+    );
+    assert!(true);
 }
