c404825c3a4ecf5be61cbe3684ef71479e38a20e
Arturo Marquez <arturomf94@gmail.com>
2020-08-21 00:20:44 -0500

    Repeated blocks func isnt useful to guess mode

--- set1/chal11/src/main.rs
+++ set1/chal11/src/main.rs
@@ -1,6 +1,8 @@
 extern crate openssl;
 use rand::Rng;
 use openssl::symm::{Cipher, Mode, Crypter};
+use std::collections::HashSet;
+use std::hash::Hash;
 
 const BLOCK_SIZE: usize = 16;
 
@@ -75,7 +77,33 @@ fn encrypt_aes_128_cbc(
     ciphertext
 }
 
-fn encryption_oracle(mes: &[u8], mode: CipherMode) -> Vec<u8> {
+fn append_random_bytes(mes: Vec<u8>) -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    let mut result: Vec<u8> = Vec::new();
+    let prefix_len = rng.gen_range(1, 6);
+    let suffix_len = rng.gen_range(1, 6);
+    for _ in 0..prefix_len {
+        result.push(rng.gen());
+    }
+    result.extend(mes);
+    for _ in 0..suffix_len {
+        result.push(rng.gen());
+    }
+    result
+}
+
+fn pkcs7(message: &[u8], block_size: usize) -> Vec<u8> {
+    let mut message = message.to_vec();
+    let mut padding_len = block_size - (message.len() % block_size);
+    if padding_len == 0 { padding_len = block_size};
+    let pad = vec![padding_len as u8; padding_len as usize];
+    message.extend(pad.iter());
+    message
+}
+
+fn encryption_oracle(mut mes: Vec<u8>, mode: CipherMode) -> Vec<u8> {
+    mes = append_random_bytes(mes);
+    mes = pkcs7(&mes, BLOCK_SIZE);
     let key = rand::thread_rng().gen::<[u8; BLOCK_SIZE]>();
     match mode {
         CipherMode::ECB => {
@@ -88,7 +116,50 @@ fn encryption_oracle(mes: &[u8], mode: CipherMode) -> Vec<u8> {
     }
 }
 
+fn has_unique_elements<T>(iter: T) -> bool
+where
+    T: IntoIterator,
+    T::Item: Eq + Hash,
+{
+    let mut uniq = HashSet::new();
+    iter.into_iter().all(move |x| uniq.insert(x))
+}
+
+fn repeated_blocks(
+    ciphertext: &[u8],
+    blocksize: usize
+) -> Result<bool, String> {
+    let c_len = ciphertext.len();
+    if c_len % blocksize != 0 {
+        return Err(
+            "The length of the ciphertext must be a multiple of the blocksize".to_string()
+        )
+    }
+    let num_blocks = ciphertext.len() / blocksize;
+    let mut blocks: Vec<&[u8]> = Vec::with_capacity(num_blocks);
+    for i in 0..num_blocks {
+        blocks.push(
+            &ciphertext[(i * blocksize)..((i + 1) * blocksize)]
+        );
+    }
+    match !has_unique_elements(blocks) {
+        false => Ok(false),
+        true => Ok(true),
+    }
+}
+
 fn main() {
-    let e = encryption_oracle(b"YELLOW SUBMARINEYELLOW SUBMARINE", CipherMode::ECB);
-    println!("{:?}", e);
+    let mes = b"YELLOW SUBMARINEYELLOW SUBMARINE".to_vec();
+    let prob = rand::thread_rng().gen::<f64>();
+    let mut mode = CipherMode::CBC;
+    if prob < 0.5 {
+        mode = CipherMode::ECB;
+    }
+    println!("{:?}", mode);
+    let e = encryption_oracle(mes, mode);
+    if repeated_blocks(&e, BLOCK_SIZE).unwrap() {
+        println!("{:?}", CipherMode::ECB);
+    } else {
+        println!("{:?}", CipherMode::CBC);
+    }
 }
