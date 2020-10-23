217d357348399eadf1b1d20c9eac860b8fe770dc
Arturo Marquez <arturomf94@gmail.com>
2020-10-22 21:08:34 -0500

    Add some comments to chal12

--- set1/chal12/src/main.rs
+++ set1/chal12/src/main.rs
@@ -8,6 +8,7 @@ const BLOCK_SIZE: usize = 16;
 const KEY: &[u8] = &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
 const PAD_STRING: &str = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
 
+/// Pad a message, given a blocksize
 fn pkcs7(
     message: &[u8],
     block_size: usize,
@@ -20,6 +21,8 @@ fn pkcs7(
     message
 }
 
+
+/// Ecnrypt a byte slice with AES-128 in ECB mode
 fn encrypt_aes_128_ecb(
     plaintext: &[u8],
     key: &[u8],
@@ -43,6 +46,8 @@ fn encrypt_aes_128_ecb(
 }
 
 
+/// Encryption oracle returns an ecnrypted message
+/// with AES-128 in ECB mode
 fn encryption_oracle_ecb(
     mut mes: Vec<u8>,
     key: &[u8],
@@ -53,6 +58,8 @@ fn encryption_oracle_ecb(
     return encrypt_aes_128_ecb(&mes, &key);
 }
 
+
+/// Check if an iter type has unique elements
 fn has_unique_elements<T>(iter: T) -> bool
 where
     T: IntoIterator,
@@ -62,6 +69,9 @@ where
     iter.into_iter().all(move |x| uniq.insert(x))
 }
 
+
+/// Check whether a given byte slice has repeated
+/// blocks of size blocksize
 fn repeated_blocks(
     ciphertext: &[u8],
     blocksize: usize
@@ -86,6 +96,10 @@ fn repeated_blocks(
 }
 
 
+/// Detect the blocksize used in the ECB
+/// encryption oracle.
+/// This doesn't make much sense, but it's
+/// what the challenge asks for
 fn detect_blocksize() -> usize {
     let mut blocksize = 0_usize;
     for bs in 2..100 {
@@ -103,12 +117,18 @@ fn detect_blocksize() -> usize {
     blocksize
 }
 
+
+/// Recover a byte with an artificially
+/// constructed message
 fn get_plaintext_byte(
     mes: Vec<u8>,
 ) -> u8 {
     42
 }
 
+
+/// Recover the full plaintext given an
+/// AES-128 ECB ecnrypted cipher
 fn get_plaintext(
     ciphertext: Vec<u8>,
     blocksize: usize,
@@ -123,6 +143,8 @@ fn get_plaintext(
     String::from_utf8(plaintext).unwrap()
 }
 
+
+/// Break the ECB
 fn break_ecb(
     ciphertext: Vec<u8>
 ) -> Result<String, String>{
