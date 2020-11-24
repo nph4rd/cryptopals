2760088d612303b4313c88969920c6927493784b
Arturo Marquez Flores <arturomf94@gmail.com>
2020-11-24 10:25:28 -0600

    Format code

--- set2/chal12/src/main.rs
+++ set2/chal12/src/main.rs
@@ -1,63 +1,47 @@
 extern crate base64;
 use base64::decode;
-use openssl::symm::{Cipher, Mode, Crypter};
-use std::collections::{HashSet, HashMap};
+use openssl::symm::{Cipher, Crypter, Mode};
+use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 
 const BLOCK_SIZE: usize = 16;
-const KEY: &[u8] = &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,];
+const KEY: &[u8] = &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 const PAD_STRING: &str = "Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK";
 
 /// Pad a message, given a blocksize
-fn pkcs7(
-    message: &[u8],
-    block_size: usize,
-) -> Vec<u8> {
+fn pkcs7(message: &[u8], block_size: usize) -> Vec<u8> {
     let mut message = message.to_vec();
     let mut padding_len = block_size - (message.len() % block_size);
-    if padding_len == 0 { padding_len = block_size};
+    if padding_len == 0 {
+        padding_len = block_size
+    };
     let pad = vec![padding_len as u8; padding_len as usize];
     message.extend(pad.iter());
     message
 }
 
-
 /// Ecnrypt a byte slice with AES-128 in ECB mode
-fn encrypt_aes_128_ecb(
-    plaintext: &[u8],
-    key: &[u8],
-) -> Vec<u8> {
-    let mut encrypter = Crypter::new(
-        Cipher::aes_128_ecb(),
-        Mode::Encrypt,
-        key,
-        None).unwrap();
+fn encrypt_aes_128_ecb(plaintext: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut encrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Encrypt, key, None).unwrap();
     encrypter.pad(false);
     let data_len = plaintext.len();
     let mut ciphertext = vec![0; data_len + BLOCK_SIZE];
     let mut count = encrypter
         .update(&plaintext[..data_len], &mut ciphertext)
         .unwrap();
-    count += encrypter
-        .finalize(&mut ciphertext[count..])
-        .unwrap();
+    count += encrypter.finalize(&mut ciphertext[count..]).unwrap();
     ciphertext.truncate(count);
     ciphertext
 }
 
-
 /// Encryption oracle returns an ecnrypted message
 /// with AES-128 in ECB mode
-fn encryption_oracle_ecb(
-    mes: &mut Vec<u8>,
-    key: &[u8],
-) -> Vec<u8> {
+fn encryption_oracle_ecb(mes: &mut Vec<u8>, key: &[u8]) -> Vec<u8> {
     let mut decoded_pad = decode(&PAD_STRING).unwrap();
     mes.append(&mut decoded_pad);
     let padded_mes = pkcs7(&mes, BLOCK_SIZE);
     return encrypt_aes_128_ecb(&padded_mes, &key);
 }
-
 
 /// Check if an iter type has unique elements
 fn has_unique_elements<T>(iter: T) -> bool
@@ -69,32 +53,23 @@ where
     iter.into_iter().all(move |x| uniq.insert(x))
 }
 
-
 /// Check whether a given byte slice has repeated
 /// blocks of size blocksize
-fn repeated_blocks(
-    ciphertext: &[u8],
-    blocksize: usize
-) -> Result<bool, String> {
+fn repeated_blocks(ciphertext: &[u8], blocksize: usize) -> Result<bool, String> {
     let c_len = ciphertext.len();
     if c_len % blocksize != 0 {
-        return Err(
-            "The length of the ciphertext must be a multiple of the blocksize".to_string()
-        )
+        return Err("The length of the ciphertext must be a multiple of the blocksize".to_string());
     }
     let num_blocks = ciphertext.len() / blocksize;
     let mut blocks: Vec<&[u8]> = Vec::with_capacity(num_blocks);
     for i in 0..num_blocks {
-        blocks.push(
-            &ciphertext[(i * blocksize)..((i + 1) * blocksize)]
-        );
+        blocks.push(&ciphertext[(i * blocksize)..((i + 1) * blocksize)]);
     }
     match !has_unique_elements(blocks) {
         false => Ok(false),
         true => Ok(true),
     }
 }
-
 
 /// Detect the blocksize used in the ECB
 /// encryption oracle.
@@ -109,33 +84,27 @@ fn detect_blocksize() -> usize {
         match repeated_blocks(&ciphertext, bs) {
             Ok(true) => {
                 blocksize = bs;
-                break
-            },
+                break;
+            }
             _ => (),
         }
     }
     blocksize
 }
 
-
 /// Builds a hash-map from the known
 /// plaintext
-fn build_dict(
-    known_plaintext: &Vec<u8>,
-    blocksize: usize,
-) -> HashMap<Vec<u8>, u8> {
+fn build_dict(known_plaintext: &Vec<u8>, blocksize: usize) -> HashMap<Vec<u8>, u8> {
     let mut dict: HashMap<Vec<u8>, u8> = HashMap::new();
     for b in 0..=255 {
         let mut mes = vec![42; blocksize * (known_plaintext.len() / blocksize + 1) - 1];
-        mes.drain(mes.len()-known_plaintext.len()..);
+        mes.drain(mes.len() - known_plaintext.len()..);
         mes.extend(known_plaintext);
         mes.push(b);
         let step = known_plaintext.len() / blocksize * blocksize;
         dict.insert(
-            encryption_oracle_ecb(&mut mes, &KEY)[
-                step..step + blocksize
-            ].to_vec(),
-            b
+            encryption_oracle_ecb(&mut mes, &KEY)[step..step + blocksize].to_vec(),
+            b,
         );
     }
     dict
@@ -143,31 +112,26 @@ fn build_dict(
 
 /// Recover the full plaintext given an
 /// AES-128 ECB ecnrypted cipher
-fn get_plaintext(
-    blocksize: usize,
-) -> String {
-    let mut plaintext: Vec<u8> = Vec::new(); 
+fn get_plaintext(blocksize: usize) -> String {
+    let mut plaintext: Vec<u8> = Vec::new();
     let oracle_length = encryption_oracle_ecb(&mut vec![], &KEY).len();
     for i in 0..oracle_length {
         let dict = build_dict(&plaintext, blocksize);
         let mut mes = vec![42; blocksize - i % blocksize - 1];
         let step = i / blocksize * blocksize;
-        let target_cipher =  encryption_oracle_ecb(&mut mes, &KEY)[
-            step..step + blocksize
-        ].to_vec();
+        let target_cipher = encryption_oracle_ecb(&mut mes, &KEY)[step..step + blocksize].to_vec();
         if dict.contains_key(&target_cipher) {
             let byte = dict[&target_cipher];
             plaintext.push(byte);
         } else {
-            break
+            break;
         }
     }
     String::from_utf8(plaintext).unwrap()
 }
 
-
 /// Break the ECB
-fn break_ecb() -> Result<String, String>{
+fn break_ecb() -> Result<String, String> {
     let blocksize = detect_blocksize();
     if blocksize == 0 {
         Err("ECB or blocksize could not be detected".to_owned())
