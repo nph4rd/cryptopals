b4f0738fb61a2c14931893a4708d274028e96fb6
Arturo Marquez <arturomf94@gmail.com>
2020-11-24 00:20:35 -0600

    Include parsing routine for challenge 13

--- set2/chal13/src/main.rs
+++ set2/chal13/src/main.rs
@@ -1,3 +1,24 @@
+use url::{Url, ParseError};
+use std::collections::HashMap;
+
+fn parse(parameters: &str) -> Result<HashMap<String, String>, ParseError> {
+    let mut url: String = "https://base.com/?".to_owned();
+    url.push_str(parameters);
+    let parsed_url = Url::parse(&url[..])?;
+    let output_object: HashMap<String, String> = parsed_url.query_pairs().into_owned().collect();
+    Ok(output_object)
+
+}
+
+#[test]
+fn test_url_parsing() -> Result<(), ParseError> {
+    let test_object = parse("foo=bar&baz=qux&zap=zazzle")?;
+    assert_eq!(test_object.get("foo").unwrap().to_owned(), "bar".to_owned());
+    assert_eq!(test_object.get("baz").unwrap().to_owned(), "qux".to_owned());
+    assert_eq!(test_object.get("zap").unwrap().to_owned(), "zazzle".to_owned());
+    Ok(())
+}
+
 fn main() {
     println!("Hello, world!");
 }
