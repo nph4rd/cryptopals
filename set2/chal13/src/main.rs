0fc5053fbd6bd0426783e7f03af31e559eac537e
Arturo Marquez <arturomf94@gmail.com>
2021-02-07 21:50:43 -0600

    Add functions to encrypt and decrypt profiles

--- set2/chal13/src/main.rs
+++ set2/chal13/src/main.rs
@@ -1,6 +1,12 @@
+use openssl::symm::{Cipher, Crypter, Mode};
 use std::collections::HashMap;
+use std::string::FromUtf8Error;
 use url::{ParseError, Url};
 
+const KEY: &[u8] = &[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+const BLOCK_SIZE: usize = 16;
+
+// Parse parameters from URL
 fn parse(parameters: &str) -> Result<HashMap<String, String>, ParseError> {
     let mut url: String = "https://base.com/?".to_owned();
     url.push_str(parameters);
@@ -9,6 +15,7 @@ fn parse(parameters: &str) -> Result<HashMap<String, String>, ParseError> {
     Ok(output_object)
 }
 
+/// Create profile for a given email address
 fn profile_for(email_address: &str) -> String {
     let email_address = &email_address.replace("&", "");
     let email_address = &email_address.replace("=", "");
@@ -16,6 +23,43 @@ fn profile_for(email_address: &str) -> String {
     result += email_address;
     result += "&uid=10&role=user";
     result
+}
+
+/// Ecnrypt a byte slice with AES-128 in ECB mode
+fn encrypt_aes_128_ecb(plaintext: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut encrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Encrypt, key, None).unwrap();
+    let data_len = plaintext.len();
+    let mut ciphertext = vec![0; data_len + BLOCK_SIZE];
+    let mut count = encrypter
+        .update(&plaintext[..data_len], &mut ciphertext)
+        .unwrap();
+    count += encrypter.finalize(&mut ciphertext[count..]).unwrap();
+    ciphertext.truncate(count);
+    ciphertext
+}
+
+/// Decrypt a byte vec with AES-128 in ECB mode
+fn decrypt_aes_128_ecb(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut decrypter = Crypter::new(Cipher::aes_128_ecb(), Mode::Decrypt, key, None).unwrap();
+    let data_len = ciphertext.len();
+    let mut plaintext = vec![0; data_len + BLOCK_SIZE];
+    let mut count = decrypter
+        .update(&ciphertext[..data_len], &mut plaintext)
+        .unwrap();
+    count += decrypter.finalize(&mut plaintext[count..]).unwrap();
+    plaintext.truncate(count);
+    plaintext
+}
+
+fn encrypt_profile(email_address: &str, key: &[u8]) -> Vec<u8> {
+    let profile = profile_for(email_address);
+    encrypt_aes_128_ecb(profile.as_bytes(), key)
+}
+
+fn decrypt_profile(ciphertext: &[u8], key: &[u8]) -> Result<String, FromUtf8Error> {
+    let plaintext = decrypt_aes_128_ecb(ciphertext, key);
+    let plaintext = String::from_utf8(plaintext)?;
+    Ok(plaintext)
 }
 
 #[test]
@@ -39,6 +83,16 @@ fn test_profile_for() {
         test_object,
         "email=foo@bar.comroleadmin&uid=10&role=user".to_owned()
     );
+}
+
+#[test]
+fn test_encrypt_decrypt_profile() -> Result<(), FromUtf8Error> {
+    let test_object = decrypt_profile(&encrypt_profile("foo@bar.com", &KEY), &KEY);
+    assert_eq!(
+        test_object.unwrap(),
+        "email=foo@bar.com&uid=10&role=user".to_owned()
+    );
+    Ok(())
 }
 
 fn main() {
